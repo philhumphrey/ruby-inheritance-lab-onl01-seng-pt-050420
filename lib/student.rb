8543dd9d59450183c60a7f1ab072fcdb489548da
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-06 22:39:22 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+KNOWLEDGE= []
 
+def initialize
+  @knowledge = KNOWLEDGE 
+ 
+end
+
+def learn(x)
+  @knowledge << x
+  end
+  
+  def knowledge
+    @knowledge
+  end  
 end
